1f19c8942069b051a09dc94629ae718f0d8fe268
bender22 <benja.duran@outlook.com>
2015-07-26 20:51:17 -0400

    change identifier for id in Receipt

--- app/models/spree/khipu_payment_receipt.rb
+++ app/models/spree/khipu_payment_receipt.rb
@@ -1,7 +1,7 @@
 module Spree
   class KhipuPaymentReceipt < ActiveRecord::Base
     before_validation :extract_payment_info
-    belongs_to :payment, foreign_key: 'transaction_id', primary_key: 'identifier' 
+    belongs_to :payment, foreign_key: 'transaction_id', primary_key: 'id' 
     
     private
     # Rellenar el receipt con la info del pago
